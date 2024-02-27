fb2bc74ae76e01f5a0dfd7d39da98b5c334ceef6
JustinianPin <justinian.pintilie@student.tuiasi.ro>
2024-02-27 12:40:56 +0200

    Primul commit JEE_test, cu afisare in info-student

---
+++ JEE-Test/src/main/java/ProcessStudentServlet.java
@@ -0,0 +1,36 @@
+import beans.StudentBean;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.File;
+import java.io.IOException;
+import java.time.Year;
+
+public class ProcessStudentServlet extends HttpServlet {
+    @Override
+    protected void doPost(HttpServletRequest request,
+                          HttpServletResponse response)
+            throws ServletException, IOException {
+// se citesc parametrii din cererea de tip POST
+        String nume = request.getParameter("nume");
+        String prenume = request.getParameter("prenume");
+        int varsta = Integer.parseInt(request.getParameter("varsta"));
+/*
+procesarea datelor - calcularea anului nasterii
+*/
+        int anCurent = Year.now().getValue();
+        int anNastere = anCurent - varsta;
+
+
+
+
+// se trimit datele primite si anul nasterii catre o alta pagina JSP pentru afisare
+        request.setAttribute("nume", nume);
+        request.setAttribute("prenume", prenume);
+        request.setAttribute("varsta", varsta);
+        request.setAttribute("anNastere", anNastere);
+        request.getRequestDispatcher("./info-student.jsp").forward(request, response);
+    }
+}
